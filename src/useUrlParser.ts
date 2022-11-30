2fd2a6f729a6c0a1b776bdea7f04fbaa3a3a62a4
varheight-klg <jens@varheight.ch>
2022-11-30 16:13:53 +0100

    Update useUrlParser

--- src/useUrlParser.ts
+++ src/useUrlParser.ts
@@ -1,3 +1,10 @@
+import { useId } from "react";
+
+type URLReplacerArgs = {
+  urls: string[];
+  parts: string[];
+};
+
 type UseURLParserResult = {
   type: "url" | "text";
   value: string;
@@ -7,30 +14,50 @@ type UseURLParserResult = {
 const urlRegex =
   /(?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])/g;
 
-export const useUrlParser = () => (text: string) => {
-  const result: UseURLParserResult[] = [];
-  const matches = text.matchAll(urlRegex);
-  const matchesArray = Array.from(matches);
-  const urls = matchesArray.flatMap((m) => m.filter(Boolean));
-  const parts = text.split(urlRegex).filter(Boolean);
+const buildReplacer = ({ urls, parts }: URLReplacerArgs) => {
+  return () => {
+    const result: UseURLParserResult[] = [];
 
-  for (let j = 0; j < Math.max(1, parts.length - 1); j++) {
-    result.push({
-      type: "text",
-      value: parts[j],
-    });
-    result.push({
-      type: "url",
-      value: urls[j],
-    });
-  }
+    for (let j = 0; j < Math.max(1, parts.length - 1); j++) {
+      result.push({
+        type: "text",
+        value: parts[j],
+      });
+      result.push({
+        type: "url",
+        value: urls[j],
+      });
+    }
 
-  if (parts.length > urls.length) {
-    result.push({
-      type: "text",
-      value: parts[parts.length - 1],
-    });
-  }
+    if (parts.length > urls.length) {
+      result.push({
+        type: "text",
+        value: parts[parts.length - 1],
+      });
+    }
 
-  return result;
+    return result;
+  };
 };
+
+export const useUrlParser =
+  () =>
+  (text: string): UseURLParserResult[] => {
+    const matches = text.matchAll(urlRegex);
+    const matchesArray = Array.from(matches);
+    const urls = matchesArray.flatMap((m) => m.filter(Boolean));
+    const parts = text.split(urlRegex).filter(Boolean);
+
+    const urlReplacer = buildReplacer({ urls, parts });
+
+    if (urls.length > 0) {
+      return urlReplacer();
+    }
+
+    return [
+      {
+        type: "text",
+        value: text,
+      },
+    ];
+  };
